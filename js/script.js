3657fa4d9743347d527aa07fe059f8990fa83d31
msicakkanli <msicakkanli@protein.tech>
2018-03-03 13:31:32 +0300

    add on click function & modified showpage funcrion

--- js/script.js
+++ js/script.js
@@ -5,34 +5,52 @@ let studentCount = student.length;
 // get student li item
 let students = document.getElementsByClassName('student-item');
 
+$(document).ready(function () {
+    showPage(1);
+})
 
-function showPage(studentNumber) {
+function showPage(clickedPage) {
     //hide studenlist
+    alert("showpage çalıştı");
     $('.student-item').hide();
     //show student items on first page
-    for (let i=0; i<studentNumber; i++) {
-        students[i].style.display = 'block';
-    }
+    let studentNumber = 10 * clickedPage;
+    alert("student number = "+studentNumber);
+    if (clickedPage === 1) {
+        for (let i=0; i<studentNumber; i++) {
+            students[i].style.display = 'block';
+        }
+    } else {
+        alert("elseye gelindi")
+        for (let i = 10*(clickedPage-1); i<studentNumber; i++){
+            students[i].style.display = 'block';   
+        }
+    }; 
 }
 
-showPage(10);
 
-function appendPageLinks() {
-    let page = 1
+
+function appendPageLinks(pageNumber) {
+    
     let pageCount = Math.ceil(studentCount / 10);
     $('.page').append('<div class="pagination"></div>');
     $('.pagination').append('<ul></ul>');
     
-    for (let i=1; i<pageCount; i++ , page++) 
+    for (let i=1; i<pageCount; i++ ) 
     {      
         $('.pagination ul').append(    
         '<li>'+
-            '<a href="#">' + page + '</a>'+
+            '<a href="#">' + i + '</a>'+
           '</li>'
     )};
-    $( ".pagination ul li:nth-child(1) a").addClass("active");
+    $( ".pagination ul li a").first().addClass("active");
 }
 
-appendPageLinks();
+appendPageLinks(3);
 
+$('.pagination ul li a').on('click',function () {
+   let clickedPage =  $(this).parent('li').index()+1;
+   alert(clickedPage + "sayfasına tuşlandı");
+showPage(clickedPage);
+})
 
